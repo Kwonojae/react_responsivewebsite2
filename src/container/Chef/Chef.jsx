1ae64fcea3dd7c3987c393428765e02f290ce3ba
Kwonojae <rnjsdhwo1325@gmail.com>
2022-07-28 05:41:45 +0900

    responsive chef complete

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { SubHeading } from '../../components'
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_image" />
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad blanditiis exercitationem, quos et consequatur earum porro quae, ea impedit deleniti animi voluptas cumque. Quas velit est voluptatem sed fuga. Optio.</p>
+        </div>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis commodi tempore adipisci unde aspernatur, quos, dolores incidunt quia fuga labore eius repellat eos sed non aliquam architecto modi sunt dolorum.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,9 +8,11 @@ const Header = () => (
     <div className='app__wrapper_info'>
       <SubHeading title="Chase the new flavour"/>
       <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa mollitia quia recusandae, debitis hic illo dolorem ipsum, corporis atque ratione eaque, temporibus voluptate. At quibusdam ratione labore, quos sequi quasi.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
     </div>
     <div className='app__wrapper_img'>
-
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
